89ba338bab117a62230c44ce5dd4905cdc59fb4f
zinackes <zinacke@gmail.com>
2026-03-03 21:03:15 +0100

    Add swagger for nest

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bodyParser: false
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Proximité')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, documentFactory);
 
   app.enableCors({
     origin: "*",
